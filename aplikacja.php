5b8f1abf68a61239d52e3d59387f6301981a0a56
DamianDybciak <88622538+DamianDybciak@users.noreply.github.com>
2023-09-14 11:42:20 +0200

    Add files via upload

---
+++ aplikacja.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <? echo"Zaloguj się ";?> 
+<form method="GET" action="aplikacja.php">
+
+
+    Login <input type="text" name="login" size="20" maxlength="25"> <br>
+    Hasło <input type="password" name="haslo" size="20" maxlength="25"> <br>
+    <input type="submit" value="Zaloguj">
+</form>
+</body>
+</html>
